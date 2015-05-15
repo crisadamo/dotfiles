8132c693f134cd07cca38b9e23a9612310727de8
Cristian Adamo <cristian.adamo@gmail.com>
2015-05-15 10:45:38 -0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,2 +1,2 @@
 alias py-web-server "python -m SimpleHTTPServer"
-alias idea "nohup ./$HOME/Applications/idea-IC-141.178.9/bin/idea.sh &"
+alias idea "./$HOME/Applications/idea-IC-141.178.9/bin/idea.sh &"
