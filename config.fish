1a2ffebb637eb47b435cf7e63d1f319b62a94b02
Cristian Adamo <cristian.adamo@gmail.com>
2015-05-21 17:03:14 -0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,2 +1,4 @@
 alias py-web-server "python -m SimpleHTTPServer"
-alias idea "./$HOME/Applications/idea-IC-141.178.9/bin/idea.sh &"
+alias idea "./$HOME/Applications/idea-IC-141.1010.3/bin/idea.sh &"
+set -xU JAVA_HOME=/usr/lib/jvm/java-8-oracle/
+
